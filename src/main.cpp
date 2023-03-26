cafad86cacfbfb8fd56d9a72e486706bcda5faee
sarahannealice <sarah.annealice@gmail.com>
2023-03-25 21:21:51 -0300

    added program loop

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,35 @@
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string firstNum;
+    string secNum;
+    int cont;
+    bool loopProgram = true;
+
+    while (loopProgram) {
+        //taking user input
+        cout << "enter a rational number: ";
+        cin >> firstNum;
+
+        cout << "enter a second rational number: ";
+        cin >> secNum;
+
+        cout << "first: " << firstNum << endl;
+        cout << "second: " << secNum << endl;
+
+        cout << "\nwould you like to continue? [1]yes [3]no" << endl;
+        cin >> cont;
+
+        if (cont == 1) {
+            continue;
+        } else if (cont == 3) {
+            //breaks while loop
+            cout << "ending program";
+            loopProgram = false;
+        } else {
+            cout << "invalid input. assuming you'd like to continue\n" << endl;
+        }
+    }//end program loop
+
     return 0;
-}
+}//end main method
